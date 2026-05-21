711accf41cab05993e993e9912a6c67f75b1732f
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2026-05-21 23:24:30 +0700

    feat: master branch

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('nik');
+            $table->string('image_path');
+            $table->string('thumb_path');
+            $table->boolean('is_active')->default(true);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
