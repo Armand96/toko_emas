f54561c27acda2866bf50f2343144f3ebb3905db
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2026-05-31 10:49:21 +0700

    feat: master customer

---
+++ app/Models/MCustomer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MCustomer extends Model
+{
+    protected $fillable = [
+        'customer_name',
+        'address',
+        'phone_number',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+}
